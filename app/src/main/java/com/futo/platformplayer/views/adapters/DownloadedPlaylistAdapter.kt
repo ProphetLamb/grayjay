69cf9aac4c6672e757af78287e917a23cfcc6e4b
ProphetLamb <prophet.lamb@gmail.com>
2024-05-11 16:15:07 +0200

    implement DownloadedPlaylistAdapter and ViewHolder

---
+++ app/src/main/java/com/futo/platformplayer/views/adapters/DownloadedPlaylistAdapter.kt
@@ -0,0 +1,71 @@
+﻿package com.futo.platformplayer.views.adapters
+
+import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.futo.platformplayer.R
+import com.futo.platformplayer.api.media.structures.TemporalItem
+import com.futo.platformplayer.fragment.mainactivity.main.DownloadsFragment
+import com.futo.platformplayer.models.PlaylistDownloadedOrWatchlist
+import com.futo.platformplayer.states.StateDownloads
+import com.futo.platformplayer.states.StatePlaylists
+import com.futo.platformplayer.views.adapters.viewholders.DownloadedPlaylistViewHolder
+
+class DownloadedPlaylistAdapter(frag: DownloadsFragment, view: View) : DownloadFilterableAdapter<PlaylistDownloadedOrWatchlist, DownloadedPlaylistViewHolder>(frag, view)  {
+    private val _listPlaylistsContainer: LinearLayout = view.findViewById(R.id.downloads_playlist_container)
+    private val _meta: TextView = view.findViewById(R.id.downloads_playlist_meta)
+    private val _listPlaylists: LinearLayout = view.findViewById(R.id.downloads_playlist_list)
+
+    init {
+        StateDownloads.instance.onDownloadedChanged.subscribe {
+            updateDataset()
+            updateContainer()
+        }
+        updateDataset()
+        updateContainer()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DownloadedPlaylistViewHolder {
+        val holder = DownloadedPlaylistViewHolder(parent)
+        return holder
+    }
+
+    override fun updateContainer() {
+        _listPlaylistsContainer.visibility = if (itemCount == 0) { GONE } else { VISIBLE }
+        val countText = if (sourceCount == itemCount) "${sourceCount}" else "${sourceCount} / ${itemCount}"
+        _meta.text = "(${countText} ${_view.context.getString(R.string.playlists).lowercase()}, ${_filteredDataset.sumOf { it.videos.size }} ${_view.context.getString(R.string.videos).lowercase()})"
+    }
+
+    override fun updateDataset() {
+         var playlistTemporal = StateDownloads.instance.getDownloadPlaylistsTemporal().mapNotNull {
+            val playlist = StatePlaylists.instance.getPlaylist(it.inner.id)
+            if (playlist == null) {
+                null
+            } else {
+                TemporalItem(PlaylistDownloadedOrWatchlist.create(it.inner, playlist), it.createdAt)
+            }
+        } + listOfNotNull(StateDownloads.instance.getWatchLaterDescriptor()).map {
+            val playlist = StatePlaylists.instance.getWatchLater()
+            TemporalItem.now(PlaylistDownloadedOrWatchlist.watchlist(it, playlist, _view.context))
+        }
+        _originalDataset = playlistTemporal
+
+        if (!query.isNullOrBlank()) {
+            val query = query!!.trim()
+            playlistTemporal = playlistTemporal
+                .filter { it.inner.name.contains(query, true) }
+        }
+        _filteredDataset = when (sortBy) {
+            0 -> playlistTemporal.sortedByDescending { it.createdAt }
+            1 -> playlistTemporal.sortedBy { it.createdAt }
+            2 -> playlistTemporal.sortedBy { it.inner.name }
+            3 -> playlistTemporal.sortedByDescending { it.inner.name }
+            else -> throw IllegalStateException("Invalid sorting algorithm selected.")
+        }.map { it.inner }
+
+        notifyDataSetChanged()
+    }
+}
---
+++ app/src/main/java/com/futo/platformplayer/views/adapters/viewholders/DownloadedPlaylistViewHolder.kt
@@ -0,0 +1,29 @@
+﻿package com.futo.platformplayer.views.adapters.viewholders
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.futo.platformplayer.R
+import com.futo.platformplayer.images.GlideHelper.Companion.crossfade
+import com.futo.platformplayer.models.PlaylistDownloadedOrWatchlist
+import com.futo.platformplayer.states.StatePlaylists
+import com.futo.platformplayer.views.adapters.AnyAdapter
+
+class DownloadedPlaylistViewHolder(view: ViewGroup) : AnyAdapter.AnyViewHolder<PlaylistDownloadedOrWatchlist>(
+    LayoutInflater.from(view.context).inflate(R.layout.list_downloaded_playlist, view, false)
+) {
+    var imageView: ImageView = _view.findViewById(R.id.downloaded_playlist_image);
+    var imageText: TextView = _view.findViewById(R.id.downloaded_playlist_name);
+
+    override fun bind(value: PlaylistDownloadedOrWatchlist) {
+        imageText.text = if (value.isWatchlist) { _view.context.getString(R.string.watch_later) } else { value.playlist!!.name }
+        val firstVideo = if (value.isWatchlist) { StatePlaylists.instance.getWatchLater() } else { value.videos }.firstOrNull()
+        Glide.with(imageView)
+            .load(firstVideo?.thumbnails)
+            .crossfade()
+            .into(imageView);
+    }
+
+}
